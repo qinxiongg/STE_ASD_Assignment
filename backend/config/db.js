11fff5679de52c288bcb447721d8ae797512eba1
QX <tanq0046@e.ntu.edu.sg>
2024-11-06 10:43:56 +0800

    final

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,19 @@
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri);
+// const uri = 'mongodb://localhost:27017';
+// const client = new MongoClient(uri);
 
-const connectDB = async () => {
-  try {
-    await client.connect();
-    console.log('Connected to MongoDB');
-    return client.db('data');
-  } catch (error) {
-    console.error('Error connecting to MongoDB', error);
-    process.exit(1);
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await client.connect();
+//     console.log('Connected to MongoDB');
+//     return client.db('data');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB', error);
+//     process.exit(1);
+//   }
+// };
 
-module.exports = {
-  connectDB,
-};
+// module.exports = {
+//   connectDB,
+// };
